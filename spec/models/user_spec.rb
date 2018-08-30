eb229d521004700227d538e55d7db31ce9601dc7
Alyson Takai <shtakai@gmail.com>
2018-08-30 20:58:47 +0700

    Modify all pending

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a first name, last name, email and password'
+
+  it 'is invalid without a first name'
+
+  it 'is invalid without a last name'
+
+  it 'is invalid without an email'
+
+  it 'is invalid with a dupulicate email address'
+
+  it 'returns a users full name as a string'
 end
