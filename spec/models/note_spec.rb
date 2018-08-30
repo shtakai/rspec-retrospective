1be90773ae7f5a3af7e49797b9b35329f3665cb4
Alyson Takai <shtakai@gmail.com>
2018-08-30 22:35:31 +0700

    Refactor more DRY

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,34 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+  before do
+    @user = User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'joepassword',
+    )
+
+    @project = @user.projects.create(
+      name: 'Test project',
+    )
+  end
 
   describe "search message for a term" do
     context 'when a match is found' do
       it 'returns notes that match the search term' do
-        user = User.create(
-          first_name: 'Joe',
-          last_name: 'Tester',
-          email: 'tester@example.com',
-          password: 'joepassword',
-        )
 
-        project = user.projects.create(
-          name: 'Test project',
-        )
-
-        note1 = project.notes.create(
+        note1 = @project.notes.create(
           message: 'This is the first note.',
-          user: user,
+          user: @user,
         )
 
-        note2 = project.notes.create(
+        note2 = @project.notes.create(
           message: 'This is the second note.',
-          user: user,
+          user: @user,
         )
 
-        note3 = project.notes.create(
+        note3 = @project.notes.create(
           message: 'First, preheat the oven.',
-          user: user,
+          user: @user,
         )
 
         expect(Note.search("first")).to include(note1, note3)
@@ -38,30 +40,19 @@ RSpec.describe Note, type: :model do
 
     context 'when no match is found' do
       it 'returns an empty collection when no results are found' do
-        user = User.create(
-          first_name: 'Joe',
-          last_name: 'Tester',
-          email: 'tester@example.com',
-          password: 'joepassword',
-        )
-
-        project = user.projects.create(
-          name: 'Test project',
-        )
-
-        note1 = project.notes.create(
+        note1 = @project.notes.create(
           message: 'This is the first note.',
-          user: user,
+          user: @user,
         )
 
-        note2 = project.notes.create(
+        note2 = @project.notes.create(
           message: 'This is the second note.',
-          user: user,
+          user: @user,
         )
 
-        note3 = project.notes.create(
+        note3 = @project.notes.create(
           message: 'First, preheat the oven.',
-          user: user,
+          user: @user,
         )
 
         expect(Note.search("message")).to be_empty
