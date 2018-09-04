e8fdfe740806cd979d2626a3e6b69d5a8756702a
Alyson Takai <shtakai@gmail.com>
2018-09-04 22:42:47 +0700

    Add other projects on factory

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -5,4 +5,25 @@ FactoryBot.define do
     due_on { 1.week.from_now }
     association :owner
   end
+
+  factory :project_due_yesterday, class: Project do
+    sequence(:name) { |n| "Project #{n}" }
+    description { "A test project" }
+    due_on { 1.day.ago }
+    association :owner
+  end
+
+  factory :project_due_today, class: Project do
+    sequence(:name) { |n| "Project #{n}" }
+    description { "A test project" }
+    due_on { Date.current.in_time_zone }
+    association :owner
+  end
+
+  factory :project_due_tomorrow, class: Project do
+    sequence(:name) { |n| "Project #{n}" }
+    description { "A test project" }
+    due_on { 1.day.from_now }
+    association :owner
+  end
 end
