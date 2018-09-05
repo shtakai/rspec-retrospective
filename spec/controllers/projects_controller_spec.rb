1e28801c632ecc338f261fef18309f78e7e15f9d
Alyson Takai <shtakai@gmail.com>
2018-09-05 20:59:04 +0700

    Refactor to include as a ___ user

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,14 +2,26 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
   describe '#index' do
-    it 'responds successfully' do
-      get :index
-      expect(response).to be_success
+    context 'as an authenticated user' do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      it 'responds successfully' do
+        sign_in @user
+        get :index
+        expect(response).to be_success
+      end
+
+      it 'returns a 200 response' do
+        sign_in @user
+        get :index
+        expect(response).to have_http_status '200'
+      end
     end
 
-    it 'returns a 200 response' do
-      get :index
-      expect(response).to have_http_status '200'
+    context 'as a guest' do
+
     end
   end
 end
