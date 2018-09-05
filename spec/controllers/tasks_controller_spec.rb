cca9b9a458f5a8818d4d88f15be78b431761b3b8
Alyson Takai <shtakai@gmail.com>
2018-09-05 22:16:11 +0700

    Add task #create specs

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -16,4 +16,32 @@ RSpec.describe TasksController, type: :controller do
      end
   end
 
+  describe '#create' do
+    it 'responds with JSON formmated output' do
+      new_task = { name: 'New test task' }
+      sign_in @user
+      post :create, format: :json,
+        params: { project_id: @project.id, task: new_task }
+      expect(response.content_type).to eq 'application/json'
+    end
+
+    it 'adds a new task to the project' do
+      new_task = { name: 'New test task' }
+      sign_in @user
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to change(@project.tasks, :count).by(1)
+    end
+
+    it 'requires authentication' do
+      new_task = { name: 'New test task' }
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to_not change(@project.tasks, :count)
+      expect(response).to_not be_success
+    end
+  end
+
 end
