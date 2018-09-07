35e57b9dc36d6a406cb36654e02380b5d1691d7a
Alyson Takai <shtakai@users.noreply.github.com>
2018-09-07 22:31:05 +0700

    Feature/dry spec (#16)
    * Add login_support
    
    * Include helper on Devise
    
    * Refactor to use Devise helper
    
    * rails g rspec:model task
    
    * Add task model spec with let
    
    * Refactor note model spec
    
    * Apply shared contest
    
    * Add content_type matcher
    
    * Refactor using have_content_type matcher
    
    * Add error messages on matcher
    
    * Apply matcher
    
    * Refactor with aggregate_failures
    
    * Refactor tasks feature spec

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,65 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before do
-    @user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'tester@example.com',
-      password: 'joepassword',
-    )
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project) }
 
-    @project = @user.projects.create(
-      name: 'Test project',
+  it 'is valid with a user, project, and message' do
+    note = Note.new(
+      message: 'This is a sample note',
+      user: user,
+      project: project,
     )
+    expect(note).to be_valid
   end
 
-  xit 'generates associated data from a factory' do
-    note = FactoryBot.create(:note)
+  it 'is invalid without a message' do
+    note = Note.new(message: nil)
+    note.valid?
+    expect(note.errors[:message]).to include "can't be blank"
   end
 
-  describe "search message for a term" do
+  describe 'search message for a term' do
+    let!(:note1) {
+      FactoryBot.create(
+        :note,
+        project: project,
+        user: user,
+        message: 'This is the first note',
+      )
+    }
+    let!(:note2) {
+      FactoryBot.create(
+        :note,
+        project: project,
+        user: user,
+        message: 'This is the second note',
+      )
+    }
+    let!(:note3) {
+      FactoryBot.create(
+        :note,
+        project: project,
+        user: user,
+        message: 'First, preheat the oven.',
+      )
+    }
+
     context 'when a match is found' do
       it 'returns notes that match the search term' do
-
-        note1 = @project.notes.create(
-          message: 'This is the first note.',
-          user: @user,
-        )
-
-        note2 = @project.notes.create(
-          message: 'This is the second note.',
-          user: @user,
-        )
-
-        note3 = @project.notes.create(
-          message: 'First, preheat the oven.',
-          user: @user,
-        )
-
-        expect(Note.search("first")).to include(note1, note3)
-        expect(Note.search("first")).to_not include(note2)
+        expect(Note.search('first')).to include(note1, note3)
       end
     end
 
     context 'when no match is found' do
-      it 'returns an empty collection when no results are found' do
-        note1 = @project.notes.create(
-          message: 'This is the first note.',
-          user: @user,
-        )
-
-        note2 = @project.notes.create(
-          message: 'This is the second note.',
-          user: @user,
-        )
-
-        note3 = @project.notes.create(
-          message: 'First, preheat the oven.',
-          user: @user,
-        )
-
-        expect(Note.search("message")).to be_empty
+      it 'returns an empty collection' do
+        expect(Note.search('message')).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
